6bb9a3a3b4b8e52d8ea9a2e65631eeb51ec62605
AJ Ashton <aj@developmentseed.org>
2011-11-22 17:46:04 -0500

    Code reorganization

--- millprep.py
+++ millprep.py
@@ -32,30 +32,11 @@ except:
 gmerc_proj4 = '"+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext +no_defs +over"'
 gmerc_bounds = "-180.0 -85.05112877980659 180 85.05112877980659"
 
-def EQUAL(a, b):
-  ## case-insensitive equality check
-  return a.lower() == b.lower()
-
 
 ################################################################################
-## REPROJECT
-
-def reproject(src_path, dst_path, output_format='shp', clip=True):
-  """
-  Reproject a file to Google Mercator. This will also clip the data if any
-  part of it falls outside the square extents of the projection unless the
-  user has specified otherwise.
-
-  src_path:  the path to the source file
-
-  dst_path:  the path to the destination file
-
-  output_sqlite:  if not, output shapefile
-
-  clip: a boolean indicating whether (True) or not (False) to attempt to clip
-  the datasource to the Google Mercator zoom level 0 tile boundary.
-  
-  """
+# REPROJECT VECTOR DATASOURCES
+#
+def vector_reproject(src_file, dst_file, output_format='shp', clip=True):
 
   ## Set up the command-line options for ogr2ogr
   ogr_cmd = [ 'ogr2ogr' ]
@@ -66,46 +47,69 @@ def reproject(src_path, dst_path, output_format='shp', clip=True):
   ogr_cmd.append('-s_srs EPSG:4326')
   ogr_cmd.append('-t_srs ' + gmerc_proj4)
   if (clip):
-    # TODO: Is there much of a performance impact to running this on datasources
-    # that don't require it? Run benchmarks and adjust code if necessary.
     ogr_cmd.append('-clipsrc ' + gmerc_bounds)
-  ogr_cmd.append('"' + dst_path + '"')
-  ogr_cmd.append('"' + src_path + '"')
+  ogr_cmd.append('"' + dst_file + '"')
+  ogr_cmd.append('"' + src_file + '"')
 
-  # shell=True is potentially unsafe, however the only approach I can get to work
-  subprocess.check_call(' '.join(ogr_cmd), shell=True)
+  # shell=True is potentially unsafe, but is the only approach I can get to work
+  subprocess.call(' '.join(ogr_cmd), shell=True)
+
+  return True
+
+
+################################################################################
+# PROCESS VECTORS INDIVIDUALLY
+#
+def vector_process(src_files, dst_dir, output_format='shp', clip=True):
+  
+  for src_file in src_files:
+
+    # process the file in its current directory if no output directory was 
+    # specified with -d
+    if not (dst_dir):
+      dst_dir = path.split(src_file)[0]
+
+    # The output filename is based on the input file, with _millready appended
+    dst_filename = path.splitext(path.basename(src_file))[0] + \
+      '_millready.' + output_format
+    dst_file = path.join(dst_dir, dst_filename)
+
+    # Do the reprojection
+    vector_reproject(src_file, dst_file, output_format, clip)
+
+    # create a Mapnik index for the shapefile, if we made a shapefile
+    if (output_format == 'shp'):
+      subprocess.call(['shapeindex', dst_file])
+
+  return True
 
 
 ################################################################################
-## SHAPEFILE INDEXING
+# MERGE VECTORS INTO A SINGLE FILE
+#
+def vector_merge(src_files, dst_merged, output_format='shp', clip=True):
+  # TODO
+  return True
 
-def shp_index(src_file):
-  """
-  Calls the Mapnik `shapeindex` utility to create .index files for any output
-  shapefiles.
-
-  """
-
-  subprocess.check_call('shapeindex ' + src_file)
 
 ################################################################################
-## MAIN
+# MAIN
 
 def main():
 
   # ---- Process command-line arguments ----------------------------------------
-
+  #
   parser = argparse.ArgumentParser(description='''Convert geographic 
     datasources to more TileMill-optimized formats.''')
 
   parser.add_argument('src_files',
-    nargs='+', 
-    help="A geographic file to optimize for TileMill")
+    nargs='+', metavar='INPUT_FILE',
+    help='''A geographic file to optimize for TileMill''')
 
   parser.add_argument('--sqlite',
     dest='output_format', action='store_const', const='sqlite', default='shp',
-    help='''NOT YET IMPLEMENTED. Use SQLite as the output file format instead 
-            of the default ESRI Shapefile''')
+    help='''Use SQLite as the output file format instead of the default,
+            ESRI Shapefile''')
 
   parser.add_argument('--noclip',
     dest='clip', action='store_false', default=True,
@@ -113,41 +117,35 @@ def main():
             bounds of the Google Mercator square.''')
 
   parser.add_argument('-d',
-    nargs=1, dest='dst_dir',
+    nargs=1, dest='dst_dir', metavar='OUTPUT_DIR',
     help='''Destination directory to output the processed files. If not
             specified, output files will be kept in the same directory as their 
             respective input files.''')
 
   parser.add_argument('-m', '--merge',
-    dest='output_merged', action='store_true',
-    help='NOT YET IMPLEMENTED. Merge all input files into a single output file.')
+    nargs=1, dest='dst_merged', metavar='MERGED_FILE',
+    help='''Merge all input files into this single output file.''')
 
   args = parser.parse_args()
 
+  # ---- send files to be merged -----------------------------------------------
+  #
+  if (args.dst_merged):
+    if (args.dst_dir):
+      print("WARNING: --merge overrides -d. Output directory ignored.")
+    vector_merge(args.src_files, args.dst_merged, args.output_format, args.clip)
 
-  # Optimizes files one at a time according to the specified options.
-  for src_file in args.src_files:
-
-    # Get the absolute path of the input file
-    src_path = path.abspath(src_file)
-
-    # The output filename is based on the input file, with _millready appended
-    dst_filename = path.splitext(path.basename(src_file))[0] + \
-      '_millready.' + args.output_format
-
-    # Define the the output directory if the user did not specify one
-    if not (args.dst_dir):
-      dst_dir = path.split(src_path)[0]
-    else:
+  # ---- send files to be processed individually -------------------------------
+  #
+  else:
+    if (args.dst_dir):
       dst_dir = args.dst_dir[0]
-    dst_path = path.join(dst_dir, dst_filename)
+    else:
+      dst_dir = None
+    vector_process(args.src_files, dst_dir, args.output_format, args.clip)
 
-    #
-    reproject(src_path, dst_path, args.output_format, args.clip)
 
-    # create a Mapnik index for the shapefile
-    #shp_index(dst_path)
-
+################################################################################
 
 if __name__ == '__main__':
   if not main():
